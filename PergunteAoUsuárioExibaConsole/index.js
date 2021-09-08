df6865a0e8ad0acfc4fee7690430237629910821
Yuri Silva <yuricosta149@gmail.com>
2021-09-08 08:57:36 -0300

    PerguntandoAoUsuarioExibindoNoConsole

---
+++ PergunteAoUsuárioExibaConsole/index.js
@@ -0,0 +1,7 @@
+let nome = prompt("Qual seu nome?")
+let age = Number(prompt("Quantos anos você tem?"))
+let sport = confirm("Gosta de praticar algum esporte?")
+
+console.log(nome)
+console.log(age)
+console.log(sport)
