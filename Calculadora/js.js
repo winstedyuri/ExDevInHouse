1ca4ea64b94af62e8762176ef353358a3b81d88a
YuriSilvaW <yuri@nexti.com>
2021-09-13 23:09:14 -0300

    Calculadora

--- Calculadora/js.js
+++ Calculadora/js.js
@@ -1,6 +1,26 @@
-var adicao
-var subtracao
-var multiplicacao
-var divisao
+var resultado = prompt("Qual operação matemática desejas realizar?\n Opção 1: Adição\n Opção 2: Subtração\n Opção 3: Multiplicação\n Opção 4: Divisão")
+var n1 = Number(prompt(`Digite o primeiro número: `))
+var n2 = Number(prompt(`Digite outro número: `))
+ 
+var soma = (n1 + n2)
+var subtracao = (n1 - n2)
+var multiplicacao = (n1 * n2)
+var divisao = (n1 / n2)
 
-prompt("Qual operação matemática desejas realizar? Digitar com caracteres")
+switch (resultado) {
+    case "1":
+        alert(soma)
+        break;
+
+    case "2":
+        alert(subtracao)
+        break;
+
+    case "3":
+        alert(multiplicacao)
+        break;
+
+    case "4":
+        alert(divisao)
+        break;
+}
---
+++ Calculadora/js2.js
@@ -0,0 +1,11 @@
+function parimpar(n){
+    if(n%2 == 0) {
+        return "Par!"
+    } else {
+        return "Ímpar!"
+    }
+}
+
+let res = parimpar(5)
+
+console.log(res)
