38334b2aa2b87f1e4e80fa16b79483daa23d6d01
YuriSilvaW <yuri@nexti.com>
2021-09-14 19:21:12 -0300

    Refatora utilizando teamplate strings e mensagem de resultado.

--- Calculadora/js.js
+++ Calculadora/js.js
@@ -1,4 +1,4 @@
-var resultado = prompt("Qual operação matemática desejas realizar?\n Opção 1: Adição\n Opção 2: Subtração\n Opção 3: Multiplicação\n Opção 4: Divisão")
+var operacao = prompt("Qual operação matemática desejas realizar?\n Opção 1: Adição\n Opção 2: Subtração\n Opção 3: Multiplicação\n Opção 4: Divisão")
 var n1 = Number(prompt(`Digite o primeiro número: `))
 var n2 = Number(prompt(`Digite outro número: `))
  
@@ -7,20 +7,20 @@ var subtracao = (n1 - n2)
 var multiplicacao = (n1 * n2)
 var divisao = (n1 / n2)
 
-switch (resultado) {
+switch (operacao) {
     case "1":
-        alert(soma)
+        alert(`O resultado é: ${soma}`)
         break;
 
     case "2":
-        alert(subtracao)
+        alert(`O resultado é: ${subtracao}`)
         break;
 
     case "3":
-        alert(multiplicacao)
+        alert(`O resultado é: ${multiplicacao}`)
         break;
 
     case "4":
-        alert(divisao)
+        alert(`O resultado é: ${divisao}`)
         break;
 }
